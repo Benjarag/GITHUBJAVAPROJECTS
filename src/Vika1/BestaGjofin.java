1b59286b7792786aa56b25af54d8e674c551cfac
Benjamín Ragnarsson <benjarag7@outlook.com>
2024-08-26 08:40:21 +0000

    eftir i kvöld sun

---
+++ src/Vika1/BestaGjofin.java
@@ -0,0 +1,73 @@
+package Vika1;
+
+// import edu.princeton.cs.algs4.StdIn;
+// import edu.princeton.cs.algs4.StdOut;
+
+// public class BestaGjofin {
+//     public static void main(String[] args) {
+//         int n = StdIn.readInt();
+//         // To consume the newline after the integer input (crucial!)
+//         StdIn.readLine(); 
+
+
+//         int highest_number = Integer.MIN_VALUE;
+//         String highest_name = "";
+
+
+       
+//         for (int i=0; i < n; i++) {
+//             // reading the whole input
+//             String input = StdIn.readLine();
+//             // splitting to get name and number
+//             String[] parts_name_n_number = input.split(" ");
+//             // getting the number
+//             int number = Integer.parseInt(parts_name_n_number[1]);
+//             // checking if its the highest number
+//             // and sets a new highest name
+//             // if it finds a higher number then then what is already
+//             if (highest_number < number) {
+//                 highest_number = number;
+//                 highest_name = parts_name_n_number[0];
+//             }
+//         }
+//         // prints the highest name
+//         StdOut.println(highest_name);
+//     }
+// }
+
+import java.util.Scanner;
+
+
+public class BestaGjofin {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        // To consume the newline after the integer input (crucial!)
+        scan.nextLine(); 
+
+
+        int highest_number = Integer.MIN_VALUE;
+        String highest_name = "";
+
+
+       
+        for (int i=0; i < n; i++) {
+            // reading the whole input
+            String input = scan.nextLine();
+            // splitting to get name and number
+            String[] parts_name_n_number = input.split(" ");
+            // getting the number
+            int number = Integer.parseInt(parts_name_n_number[1]);
+            // checking if its the highest number
+            // and sets a new highest name
+            // if it finds a higher number then then what is already
+            if (highest_number < number) {
+                highest_number = number;
+                highest_name = parts_name_n_number[0];
+            }
+        }
+        // prints the highest name
+        System.out.println(highest_name);
+    }
+}
--- src/invert2.java
+++ src/invert2.java
@@ -3,53 +3,23 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.Stopwatch;
 
-public class invert2 {
-    public static void main(String[] args) {
-        // Reading the first integer which represents the size of the array
-        // int n = StdIn.readInt();
-        int n = 200000;
-        int[] A = new int[n];
-        // Reading the elements of the array
-        for (int i = 0; i < n; i++) {
-            A[i] = i;
-            // A[i] = StdIn.readInt();
-        }
-        StdRandom.shuffle(A);
-        
-        // Creating and filling array B
-
-        Stopwatch stopwatch = new Stopwatch();
-
-        int[] B = new int[n];
-        for (int i=0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (A[j] == i) {
-                    B[i] = j;
-                }
-            }
-        }
-        double time = stopwatch.elapsedTime();
-        StdOut.println("n = "+n+ " Time spent "+time);
-        // // Printing the elements of array B in a single line
-        // for (int i=0; i < n; i++) {
-        //     StdOut.print(B[i] + " ");
-        // }
-        // // Add a newline after all elements are printed
-    }
-}
-
-
-
 // public class invert2 {
 //     public static void main(String[] args) {
 //         // Reading the first integer which represents the size of the array
-//         int n = StdIn.readInt();
+//         // int n = StdIn.readInt();
+//         int n = 200000;
 //         int[] A = new int[n];
 //         // Reading the elements of the array
 //         for (int i = 0; i < n; i++) {
-//             A[i] = StdIn.readInt();
+//             A[i] = i;
+//             // A[i] = StdIn.readInt();
 //         }
+//         StdRandom.shuffle(A);
+        
 //         // Creating and filling array B
+
+//         Stopwatch stopwatch = new Stopwatch();
+
 //         int[] B = new int[n];
 //         for (int i=0; i < n; i++) {
 //             for (int j = 0; j < n; j++) {
@@ -58,11 +28,41 @@ public class invert2 {
 //                 }
 //             }
 //         }
-//         // Printing the elements of array B in a single line
-//         for (int i=0; i < n; i++) {
-//             StdOut.print(B[i] + " ");
-//         }
-//         // Add a newline after all elements are printed
-//         StdOut.println();
+//         double time = stopwatch.elapsedTime();
+//         StdOut.println("n = "+n+ " Time spent "+time);
+//         // // Printing the elements of array B in a single line
+//         // for (int i=0; i < n; i++) {
+//         //     StdOut.print(B[i] + " ");
+//         // }
+//         // // Add a newline after all elements are printed
 //     }
 // }
+
+
+
+public class invert2 {
+    public static void main(String[] args) {
+        // Reading the first integer which represents the size of the array
+        int n = StdIn.readInt();
+        int[] A = new int[n];
+        // Reading the elements of the array
+        for (int i = 0; i < n; i++) {
+            A[i] = StdIn.readInt();
+        }
+        // Creating and filling array B
+        int[] B = new int[n];
+        for (int i=0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (A[j] == i) {
+                    B[i] = j;
+                }
+            }
+        }
+        // Printing the elements of array B in a single line
+        for (int i=0; i < n; i++) {
+            StdOut.print(B[i] + " ");
+        }
+        // Add a newline after all elements are printed
+        StdOut.println();
+    }
+}
